56c32f6817d8aa4464bc26c4e067d6eadba0b955
gordon1991112000 <gordon19911172000@gmail.com>
2022-05-18 17:53:30 +0800

    init

---
+++ ECSE.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+##################
+# Functions
+##################
+
+####### Send messgae to specific telegram group #######
+function notification(){
+   #curl -s –max-time 10 -d "chat_id=-342387488&disable_web_page_preview=1&text=`echo -e '\U0001F198'`$1" https://api.telegram.org/bot731592033:AAGyaKK_bxK-mDtJgijwWE5Ldz16WN0pSKE/sendMessage
+   curl -s –max-time 10 -d "chat_id=-656722013&disable_web_page_preview=1&text=`echo -e '\U0001F198'`$1" https://api.telegram.org/bot5323535871:AAH3Q0rLrNg6GMwJDXgj83izEVhKf81S5rY/sendMessage
+}
+
+####### Check any SSH login not using office gateway IP #######
+function check_ssh_ip(){
+   SUBJECT="Abnormal IP try to login server : `date --date='yesterday' '+%b %e'`"
+   MESSAGE="/tmp/check-ssh-ip-logs.txt"
+   login_ip="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
+   login_name="$(whoami)"
+
+   if [ $login_ip != 210.3.244.178 ] || [ $login_ip != 203.80.242.177 ]
+   then
+      echo  "$SUBJECT" >> $MESSAGE
+      echo  "Hostname: `hostname`" >> $MESSAGE
+      echo -e "\n" >> $MESSAGE
+      echo -e "IP address: $login_ip tried login with user $login_name" >> $MESSAGE
+      telegram=`cat $MESSAGE`
+      notification "$telegram"
+   else
+      echo "Okay" >> $MESSAGE
+   fi
+   rm $MESSAGE
+}
+
+####### Check any new user added ####### 
+function check_useradd(){
+   prev_count=0
+   count=$(grep -i "`date --date='yesterday' '+%b %e'`" /var/log/secure | egrep -wi 'useradd' | wc -l)
+   
+   if [ "$prev_count" -lt "$count" ] ; then
+      SUBJECT="New User Account is created on server : `date --date='yesterday' '+%b %e'`"
+      MESSAGE="/tmp/new-user-logs.txt"
+
+      echo  "$SUBJECT" >> $MESSAGE
+      echo  "Hostname: `hostname`" >> $MESSAGE
+      echo -e "\n" >> $MESSAGE
+      echo "The New User Details are below." >> $MESSAGE
+      echo "+------------------------------+" >> $MESSAGE
+      grep -i "`date --date='yesterday' '+%b %e'`" /var/log/secure | egrep -wi 'useradd' | grep -v 'failed adding'| awk '{print $4,$8}' | uniq | sed 's/,/ /' >>  $MESSAGE
+      echo "+------------------------------+" >> $MESSAGE
+      telegram=`cat $MESSAGE`
+      notification "$telegram"
+      rm $MESSAGE
+   fi
+}
+
+####### Check any user login except root/eclass/junior #######
+function check_user_login(){
+   prev_count=0
+   count=$(grep -i "`date --date='yesterday' '+%b %e'`" /var/log/secure | egrep -wi 'pam_unix' | grep -oP '(?<=user )[^ ]*' | grep -v root | grep -v eclass | wc -l)
+   
+   if [ "$prev_count" -lt "$count" ] ; then
+      SUBJECT="Abnormal user Account login server : `date --date='yesterday' '+%b %e'`"
+      MESSAGE="/tmp/abnormal-user--login-logs.txt"
+
+      echo  "$SUBJECT" >> $MESSAGE
+      echo  "Hostname: `hostname`" >> $MESSAGE
+      echo -e "\n" >> $MESSAGE
+      echo "The User Details are below." >> $MESSAGE
+      echo "+------------------------------+" >> $MESSAGE
+      grep -i "`date --date='yesterday' '+%b %e'`" /var/log/secure | egrep -wi 'pam_unix\(sshd' | grep -oP '(?<=user )[^ ]*' | grep -v root | grep -v eclass | grep -v junior | uniq >>  $MESSAGE
+      echo "+------------------------------+" >> $MESSAGE
+      telegram=`cat $MESSAGE`
+      notification "$telegram"
+      rm $MESSAGE
+   fi
+}
+
+####### Check /usr/local/x-ui exist or not #######
+function check_x-ui(){
+   SUBJECT="x-ui directory FOUND : `date --date='yesterday' '+%b %e'`"
+   MESSAGE="/tmp/check-x-ui-logs.txt"
+   last_log="/tmp/check_last_log.txt"
+
+   if [ -d /usr/local/x-ui ]
+   then
+      echo  "$SUBJECT" >> $MESSAGE
+      echo  "Hostname: `hostname`" >> $MESSAGE
+      echo -e "\n" >> $MESSAGE
+      echo "/usr/local/x-ui is found Please check." >> $MESSAGE
+      telegram=`cat $MESSAGE`
+      notification "$telegram"
+      last | grep pts >> $last_log
+      /home/telegram/tgsend.sh sendf $last_log
+      rm $last_log
+   else
+      echo "Okay" >> $MESSAGE
+   fi
+   rm $MESSAGE
+}
+
+function Check_Privilege(){
+  if [ `id -u` -ne 0 ]; then
+        echo "You need root privileges to run this script."
+        exit 0
+  fi
+}
+
+#################
+#Main
+#################
+Check_Privilege
+while getopts "iulx" opt; do
+   case ${opt} in
+      i) check_ssh_ip
+            ;;
+      u) check_useradd
+            ;;
+      l) check_user_login
+            ;;
+      x) check_x-ui
+            ;; 
+   esac
+done
+
+
---
+++ tgsend.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+#########################################################
+# send message to your own Telegram Bot API
+# using BotFather
+# https://core.telegram.org/bots#3-how-do-i-create-a-bot
+# https://core.telegram.org/bots/api#sendmessage
+# https://core.telegram.org/bots/api#editmessagetext
+#
+# requires jq json tool install:
+#
+# apt-get -y install jq
+# yum -y install jq
+#
+#########################################################
+# create and populate tgtoken.ini file in same
+# directory as tgsend.sh with the following:
+#
+# tgtoken='YOUR_TELEGRAM_BOT_API_TOKEN'
+# tgchatid='YOUR_TELEGRAM_CHATID'
+# tgapi="https://api.telegram.org/bot$tgtoken"
+#
+# if you don't know your Telegram chat id, you can leave
+# it empty and script will derive the chat id from the
+# Telegram API via your Bot API token credentials
+#########################################################
+tg_debug='n'
+# insert message_id into messages via editMessageText
+# https://core.telegram.org/bots/api#editmessagetext
+tg_addmsgid='y'
+# whether telegram notifications are enabled
+tg_notifications='y'
+# disable/enable web previews
+tg_webpreview='n'
+# curl --max-time and --connect-timeout for transfer/connect
+tg_timeout='10'
+
+script_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+#########################################################
+if [ -f "${script_dir}/tgtoken.ini" ]; then
+  . "${script_dir}/tgtoken.ini"
+fi
+if [ -f "/etc/centminmod/tgtoken.ini" ]; then
+  . "/etc/centminmod/tgtoken.ini"
+fi
+
+if [ -z "$tgchatid" ]; then
+  tgchatid=$(curl -4s "$tgapi/getUpdates" | jq -r '.result[].message.chat.id')
+fi
+
+tg_send() {
+  format=$1
+  message="$2"
+  silent="$3"
+  update="$4"
+  input_msgid="$5"
+  tg_type='sendMessage'
+
+  if [[ "$format" = 'md' ]]; then
+    format_opt=' -d parse_mode=MarkdownV2'
+  elif [[ "$format" = 'html' ]]; then
+    format_opt=' -d parse_mode=HTML'
+  else
+    format_opt=
+  fi
+  if [[ "$tg_notifications" = [nN] || "$silent" = 'quiet' ]]; then
+    notify_opt=' -d "disable_notification=true"'
+  else
+    notify_opt=' -d "disable_notification=false"'
+  fi
+  if [[ "$tg_webpreview" = [yY] ]]; then
+    webpreview_opt=' -d "disable_web_page_preview=false"'
+  else
+    webpreview_opt=' -d "disable_web_page_preview=true"'
+  fi
+
+  if [[ "$tg_debug" = [yY] ]]; then
+    # debug mode tg_debug='y' outputs the full json output
+    if [[ "$update" = 'update' ]]; then
+      # update an existing message_id
+      append_text="[msgid: $input_msgid Updated: $(date +"%a %d-%b-%y %T %Z")]  $message"
+      tg_type='editMessageText'
+      msgchar_count=$(echo $append_text | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$append_text" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d message_id="$input_msgid" -d chat_id="$tgchatid" -d text="$append_text" |  jq -r)
+    else
+      # send a new message
+      msgchar_count=$(echo $message | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$message" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d chat_id="$tgchatid" -d text="$message" |  jq -r)
+    fi
+    msgid=$(echo "$json_output" | jq -r '.result.message_id')
+    if [[ "$tg_addmsgid" = [yY] && "$update" != 'update' ]]; then
+      # append message_id to newly sent messages to make it easier to track messages
+      # and be able to edit and update existing messages in future
+      append_text="[msgid: $msgid] $message"
+      tg_type='editMessageText'
+      msgchar_count=$(echo $append_text | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$append_text" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d message_id="$msgid" -d chat_id="$tgchatid" -d text="$append_text" |  jq -r)
+    fi
+    echo "$json_output"
+    echo
+    echo "message_id: $msgid"
+    echo "message_char_count: $msgchar_count"
+    echo "message count: $msg_count"
+  else
+    # tg_debug='n' only outputs a compact json summary
+    if [[ "$update" = 'update' ]]; then
+      # update an existing message_id
+      append_text="[msgid: $input_msgid Updated: $(date +"%a %d-%b-%y %T %Z")]  $message"
+      tg_type='editMessageText'
+      msgchar_count=$(echo $append_text | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$append_text" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d message_id="$input_msgid" -d chat_id="$tgchatid" -d text="$append_text")
+    else
+      # send a new message
+      msgchar_count=$(echo $message | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$message" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d chat_id="$tgchatid" -d text="$message")
+    fi
+    msgid=$(echo "$json_output" | jq -r '.result.message_id')
+    if [[ "$tg_addmsgid" = [yY] && "$update" != 'update' ]]; then
+      # append message_id to newly sent messages to make it easier to track messages
+      # and be able to edit and update existing messages in future
+      append_text="[msgid: $msgid] $message"
+      tg_type='editMessageText'
+      msgchar_count=$(echo $append_text | wc -m)
+      # calculate number of 4090 character messages needed to
+      # send the entire message
+      msg_count=$(echo "($(echo "$append_text" | wc -m)+4090-1) / 4090" | bc)
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -X POST "$tgapi/${tg_type}"${notify_opt}${webpreview_opt}${format_opt} -d message_id="$msgid" -d chat_id="$tgchatid" -d text="$append_text" |  jq -r)
+    fi
+    echo "$json_output" | jq -r '.result | {from: .from.first_name, to: .chat.first_name, date: .edit_date | todate, message: .text }'
+  fi
+}
+
+tg_sendf() {
+  format=$1
+  file="$2"
+  silent="$3"
+  tg_type='sendDocument'
+
+  if [[ "$format" = 'file' && -f "$file" ]]; then
+    file="$file"
+    if [[ "$tg_notifications" = [nN] || "$silent" = 'quiet' ]]; then
+      notify_opt=' -d "disable_notification=true"'
+    else
+      notify_opt=' -d "disable_notification=false"'
+    fi
+    if [[ "$tg_debug" = [yY] ]]; then
+      # debug mode tg_debug='y' outputs the full json output
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -F document=@"$file" "$tgapi/${tg_type}?chat_id=$tgchatid" |  jq -r)
+      echo "$json_output"
+    else
+      # tg_debug='n' only outputs a compact json summary
+      json_output=$(curl -4s --connect-timeout $tg_timeout --max-time $tg_timeout -F document=@"$file" "$tgapi/${tg_type}?chat_id=$tgchatid" |  jq -r '.result | {from: .from.first_name, to: .chat.first_name, date: .date | todate, document: .document.file_name, mime: .document.mime_type, size: .document.file_size }')
+      echo "$json_output"
+    fi
+  else
+    echo "file not found: $file"
+  fi
+}
+
+help() {
+  echo
+  echo "Usage:"
+  echo
+  echo "$0 send \"your message in double quotes\""
+  # echo "$0 sendmd \"your message in double quotes\""
+  echo "$0 sendhtml \"your message in double quotes\""
+  echo
+  echo "disable notifications:"
+  echo
+  echo "$0 sendq \"your message in double quotes\""
+  # echo "$0 sendmdq \"your message in double quotes\""
+  echo "$0 sendhtmlq \"your message in double quotes\""
+  echo
+  echo "update existing message with message_id:"
+  echo
+  echo "$0 update \"your message in double quotes\" message_id"
+  # echo "$0 updatemd \"your message in double quotes\" message_id"
+  echo "$0 updatehtml \"your message in double quotes\" message_id"
+  echo
+  echo "update existing message with message_id & disable notifications:"
+  echo
+  echo "$0 updateq \"your message in double quotes\" message_id"
+  # echo "$0 updatemdq \"your message in double quotes\" message_id"
+  echo "$0 updatehtmlq \"your message in double quotes\" message_id"
+  echo
+  echo "send file"
+  echo
+  echo "$0 sendf filename"
+}
+
+case "$1" in
+  sendf )
+    tg_sendf file "$2"
+    ;;
+  send )
+    tg_send txt "$2" verbose
+    ;;
+  sendmd )
+    tg_send md "$2" verbose
+    ;;
+  sendhtml )
+    tg_send html "$2" verbose
+    ;;
+  sendq )
+    tg_send txt "$2" quiet
+    ;;
+  sendmdq )
+    tg_send md "$2" quiet
+    ;;
+  sendhtmlq )
+    tg_send html "$2" quiet
+    ;;
+  update )
+    tg_send txt "$2" verbose update "$3"
+    ;;
+  updatemd )
+    tg_send md "$2" verbose update "$3"
+    ;;
+  updatehtml )
+    tg_send html "$2" verbose update "$3"
+    ;;
+  updateq )
+    tg_send txt "$2" quiet update "$3"
+    ;;
+  updatemdq )
+    tg_send md "$2" quiet update "$3"
+    ;;
+  updatehtmlq )
+    tg_send html "$2" quiet update "$3"
+    ;;
+  * )
+    help
+    ;;
+esac
